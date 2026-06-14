d5c7258fcf46620b693b3bdd5aa9e15ec673041e
Urbautz <40495078+Urbautz@users.noreply.github.com>
2026-06-14 09:21:15 +0200

    Fix randomnes

--- DiggyDiggy/js/main.js
+++ DiggyDiggy/js/main.js
@@ -720,11 +720,7 @@ function openWelcome() {
 // ============================================================================
 
 function generateUUID() {
-    if (typeof crypto !== 'undefined' && crypto.randomUUID) return crypto.randomUUID();
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-        const r = Math.random() * 16 | 0;
-        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
+    return crypto.randomUUID();
 }
 
 window.setStatsPref = function setStatsPref(send) {
